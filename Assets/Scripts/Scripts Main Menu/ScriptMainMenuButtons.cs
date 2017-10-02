c15bc47c823b8fd7d75731146a08d8b72d54b5b6
Daniel Johannsson <d12@ru.is>
2017-10-02 23:07:13 +0000

    Added first scenes, main menu and button functionality

---
+++ Assets/Scripts/Scripts Main Menu/ScriptMainMenuButtons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptMainMenuButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Loads the next game scene
+    public void OnPressStartButton()
+    {
+        SceneManager.LoadScene("Scene Next", LoadSceneMode.Additive);
+    }
+
+    // Quits the application
+    public void OnPressQuitButton()
+    {
+        Application.Quit();
+    }
+}
