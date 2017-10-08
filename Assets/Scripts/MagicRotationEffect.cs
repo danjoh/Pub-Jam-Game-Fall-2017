8d8e11b3198bf68a50635dc960c198e50e654d75
Daniel Johannsson <d12@ru.is>
2017-10-08 16:15:49 +0000

    Added new Textures for magic effects, filled in a bit of background

---
+++ Assets/Scripts/MagicRotationEffect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicRotationEffect : MonoBehaviour {
+    public float rotationSpeed = 10.0f;
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward * -rotationSpeed);
+    }
+}
